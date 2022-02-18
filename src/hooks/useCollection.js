2e7af8a3631801f3a63960ba22cbfbb4fe626810
giammacarioca <giammattey@gmail.com>
2022-02-18 17:49:13 -0300

    add firestore queries

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,15 +1,22 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 // firebase imports
 import { db } from '../firebase/config'
-import { collection, onSnapshot } from 'firebase/firestore'
+import { collection, onSnapshot, query, where } from 'firebase/firestore'
 
 // c is for collection; in this case our collection is books
-export const useCollection = (c) => {
+export const useCollection = (c, _q) => {
   const [documents, setDocuments] = useState(null)
+
+  // set up query
+  const q = useRef(_q).current // an array of elements
 
   useEffect(() => {
     let ref = collection(db, c)
+
+    if (q) {
+      ref = query(ref, where(...q))
+    }
 
     // the callback function fires ever single time there's a change in the collection
     const unsubscribe = onSnapshot(ref, (snapshot) => {
@@ -21,7 +28,7 @@ export const useCollection = (c) => {
     })
 
     return () => unsubscribe()
-  }, [c])
+  }, [c, q])
 
   return { documents }
 }
